a914baef31550dae695985de42865c68a9b32842
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2021-08-28 02:20:49 +0300

    Update AddUsers.ps1

--- AddUsers/AddUsers.ps1
+++ AddUsers/AddUsers.ps1
@@ -31,34 +31,33 @@ $users = Import-csv $filepath
 
 $parent = Read-Host -Prompt "Enter parent group"
 if ($parent -eq "") {
-    Write-Host "You chose NO PARENT GROUP"
-    #Loop through the file and get user information.
-#If the user is already configured then a message will pop
-#and the script will add the user to AD and designated groups and
-#move on to the next one.
-
-foreach ($User in $users){	
-    
-    $firstname = $User.Firstname
-    $lastname = $User.Lastname
-    $username = $User.SamAccountName
-	$password = $User.Password
-    $group = $User.Group
-	$OU = $User.ou  
-   
-    $members = Get-ADGroupMember -Identity $User.Group -Recursive | select -ExpandProperty Name
-               
-    if (Get-ADUser -F { SamAccountName -eq $username }) {
         
-        Write-Warning "A user account with username $username already exists in Active Directory."
+        #Loop through the file and get user information.
+        #If the user is already configured then a message will pop
+        #and the script will add the user to AD and designated groups and
+        #move on to the next one.
+	foreach ($User in $users){	
 
-        if ($members -like $username) {
-            Write-Host "$username exsists in $group!" }
+	    $firstname = $User.Firstname
+	    $lastname = $User.Lastname
+	    $username = $User.SamAccountName
+		$password = $User.Password
+	    $group = $User.Group
+		$OU = $User.ou  
 
-        else {  
-            Add-ADGroupMember $group -Members $username
-            }
-    }
+	    $members = Get-ADGroupMember -Identity $User.Group -Recursive | select -ExpandProperty Name
+
+	    if (Get-ADUser -F { SamAccountName -eq $username }) {
+
+		Write-Warning "A user account with username $username already exists in Active Directory."
+
+		if ($members -like $username) {
+		    Write-Host "$username exsists in $group!" }
+
+		else {  
+		    Add-ADGroupMember $group -Members $username
+		    }
+	    }
           
 
     else {
